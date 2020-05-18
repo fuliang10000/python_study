6f8d373c39f82a1456a867099d9ce5abd5be3e5b
付亮 <fuliang19890809@163.com>
2020-05-18 10:55:04 +0800

    提交 Signed-off-by: 付亮 <fuliang19890809@163.com>

--- index.py
+++ index.py
@@ -1,5 +1,14 @@
-def myfunc(n):
-  return lambda a : a * n
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 
-mydoubler = myfunc(2)
-print(mydoubler(11))
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+# 使用 Person 来创建对象，然后执行 printname 方法：
+
+class Student(Person):
+  pass
+x = Student("Bill", "Gates")
+x.printname()
